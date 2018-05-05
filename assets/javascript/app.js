f7861f600dd2f5b929275a0f7e1bf4bd761b0c7d
kstoneham <kevin.g.stoneham@gmail.com>
2018-05-05 00:36:23 -0500

    threw in towel again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,27 @@
 // GLOBAL VARIABLES
 //----------------------------------------------------
-// QUESTION VARIABLES
-// var questions = {
-//     question1: {
-//         q: "What was the Beatles' first album release?",
-//         a: "Please Please Me"
-//     },
-//     question2: {
-        
-//     },
-//     question3: {
-        
-//     },
-//     question4: {
-        
-//     },
-//     question5: {
-        
-//     },
-//     question6: {
-        
-//     },
-//     question7: {
-        
-//     },
-//     question8: {
-        
-//     },
-//     question9: {
-        
-//     },
-//     question10: {
-        
-//     }
-// }
 // Starting statistics are 0
 var correct = 0;
 // 
 //----------------------------------------------------
 
 
-
-
 // FUNCTIONS
 //----------------------------------------------------
 // START GAME
+$("#startGame").click(function(){
+    $(this).hide();
+    $("#main-container").show();
+    setTimeout(endGame, 1000 * 30);
+})
 
 // END GAME- evaluate answers to questions and add to totals
 function endGame() {
     alert("Time's Up!");
+    // TODO: EVALUATE ANSWERS TO QUESTIONS WITH IF STATEMENTS
+
     $("#score").html("You got " + correct + " questions right!");
 }
-
 //----------------------------------------------------
 
 
@@ -59,12 +29,6 @@ function endGame() {
 
 // MAIN PROCESS
 //----------------------------------------------------
-// Starts the game by clicking the button, giving the player 30 seconds to answer questions
-$("#startGame").click(function(){
-    $(this).hide();
-    $("#main-container").show();
-    setTimeout(endGame, 1000 * 10);
-})
 
 
 //----------------------------------------------------
