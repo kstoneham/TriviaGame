19b966fa178784de757e261ec97e441f10d9079d
kstoneham <kevin.g.stoneham@gmail.com>
2018-05-05 09:58:05 -0500

    fixed everything

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,13 +12,29 @@ var correct = 0;
 $("#startGame").click(function(){
     $(this).hide();
     $("#main-container").show();
-    setTimeout(endGame, 1000 * 30);
+    setTimeout(endGame, 1000 * 10);
 })
 
 // END GAME- evaluate answers to questions and add to totals
 function endGame() {
     alert("Time's Up!");
     // TODO: EVALUATE ANSWERS TO QUESTIONS WITH IF STATEMENTS
+    if($('#question1').is(':checked')){
+        correct++;
+    }
+    if($('#question2').is(':checked')){
+        correct++;
+    }
+    if($('#question3').is(':checked')){
+        correct++;
+    }
+    if($('#question4').is(':checked')){
+        correct++;
+    }
+    if($('#question5').is(':checked')){
+        correct++;
+    }
+
 
     $("#score").html("You got " + correct + " questions right!");
 }
