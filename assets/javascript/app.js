227a8874b769db3e82046dd9f0b92c08ecf16a48
kstoneham <kevin.g.stoneham@gmail.com>
2018-05-04 23:17:53 -0500

    saving

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+// GLOBAL VARIABLES
+//----------------------------------------------------
+// QUESTION VARIABLES
+// var questions = {
+//     question1: {
+//         q: "What was the Beatles' first album release?",
+//         a: "Please Please Me"
+//     },
+//     question2: {
+        
+//     },
+//     question3: {
+        
+//     },
+//     question4: {
+        
+//     },
+//     question5: {
+        
+//     },
+//     question6: {
+        
+//     },
+//     question7: {
+        
+//     },
+//     question8: {
+        
+//     },
+//     question9: {
+        
+//     },
+//     question10: {
+        
+//     }
+// }
+// Starting statistics are 0
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+// 
+//----------------------------------------------------
+
+
+
+
+// FUNCTIONS
+//----------------------------------------------------
+// START GAME
+
+// END GAME
+function endGame() {
+    
+}
+
+//----------------------------------------------------
+
+
+
+
+// MAIN PROCESS
+//----------------------------------------------------
+// Starts the game by clicking the button, giving the player 30 seconds to answer questions
+$("#startGame").click(function(){
+    var timeLeft = setTimeout(endGame, 1000 * 30);
+})
+
+
+//----------------------------------------------------
